789b08b362ec6adb322ce97b30e75321c159724f
pife00 <pife00@gmail.com>
2022-12-15 16:04:06 -0500

    works

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from pollo_alimento import toSend
-from flask import escape,jsonify
+from flask import escape, jsonify
+from flask_cors import cross_origin
+
 import functions_framework
 
+@cross_origin()
 @functions_framework.http
-def comida_http(request): 
+def comida_http(request):
     return jsonify(toSend)
